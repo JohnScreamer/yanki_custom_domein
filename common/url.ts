c8c48304a0d85c1e7e8a94c3f8a51b331f108bd5
JohnScreamer <bogaturscreamer@gmail.com>
2023-01-31 18:19:53 +0200

    first commit

--- common/url.ts
+++ common/url.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://yankisli.herokuapp.com"; //https://yankisli.herokuapp.com //http://localhost:4000
+export const BASE_URL = "https://yankils.herokuapp.com"; //https://yankisli.herokuapp.com //http://localhost:4000
